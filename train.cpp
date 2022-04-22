6529c90f57084dc673e72af292e353b6819a4589
Jay-Feng2008 <101462429+Jay-Feng2008@users.noreply.github.com>
2022-04-22 17:13:10 +0800

    Update train.cpp

--- train.cpp
+++ train.cpp
@@ -25,8 +25,6 @@ float weightFunc_tanh(float weight, float delta, float input){
 int main(){
     srand(time(NULL));
     std::ofstream myfile;
-    std::ofstream lossFile;
-    lossFile.open("Loss.txt");
     for(int t = 1; t <= 200000; t++){
         data_pointer = (t%data_size)*100;   // set the data_pointer to a spcific group of data
         sum = 0;
@@ -35,7 +33,6 @@ int main(){
         }
 
         layer3 = tanh(sum);      //apply the activation function to the calculated sum
-        lossFile << (data_lable[data_pointer/100]*2-1-layer3)*(data_lable[data_pointer/100]*2-1-layer3) << "\n";
 
         for (int j = 0;j < 100;j++){
             weight[j] = weightFunc_tanh(weight[j],(float)(data_lable[data_pointer/100]*2-1)-layer3,data_list[data_pointer+j]); // updating the weights by gradient decent.
@@ -44,7 +41,6 @@ int main(){
 
     myfile.open ("weight.cpp");myfile << "float weight[100] = {"; for (double i : weight) myfile << i <<",";myfile << "};";    //rewrite the weight.cpp file to update stored weights
     myfile.close();     
-    lossFile.close();
 
     return 0;
 }
